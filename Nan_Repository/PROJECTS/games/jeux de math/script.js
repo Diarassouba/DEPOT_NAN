7cff469cb8d7aa6464b89721d03dcaaf62fb05f0
diarassouba mamadou <couda.dm@gmail.com>
2022-05-17 10:02:56 +0000

    jeux

--- Nan_Repository/PROJECTS/games/jeux de math/script.js
+++ Nan_Repository/PROJECTS/games/jeux de math/script.js
@@ -111,8 +111,3 @@
 // 	let number = Math.floor(Math.random()*tab.length)
 // 	return tab[number]
 // }
-let elt = document.getElementById('main');
-elt.innerHTML = "<ul><li>Elément 1</li><li>Elément 2</li></ul>";
-elt.style.color = "#fff";      // Change la couleur du texte de l'élément à blanche
-elt.style.backgroundColor = "#000"; // Change la couleur de fond de l'élément en noir
-elt.style.fontWeight = "bold"; // Met le texte de l'élément en gras
