2d5011f3353c8bb3eb5265df9a76e2e83706dab6
diarassouba mamadou <couda.dm@gmail.com>
2022-05-19 09:06:04 +0000

    ma connection

---
+++ CONNEXION/Connexion.js
@@ -0,0 +1,34 @@
+let MyDiv= document.getElementById("MyDiv");
+let Div= document.getElementById("Div");
+
+let connect= document.querySelector("#Cconnet")
+let inscrip= document.querySelector("#inscrip")
+
+let connection= document.querySelector("#conncete")
+let inscription= document.querySelector("#inscription")
+
+connect.addEventListener("click", function()
+{
+    Div.style.display ="block";
+    MyDiv.style.display ="none";
+}
+)
+
+inscrip.addEventListener("click", function()
+{
+    Div.style.display ="none";
+    MyDiv.style.display ="block";
+}
+)
+connection.addEventListener("click", function()
+{
+    Div.style.display ="block";
+    MyDiv.style.display ="none";
+}
+)
+inscription.addEventListener("click", function()
+{
+    Div.style.display ="none";
+    MyDiv.style.display ="block";
+}
+)
