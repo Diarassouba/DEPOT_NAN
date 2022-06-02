f5d03d71d6b33b4c1ad37ad44536c8ab32b8edc0
diarassouba mamadou <couda.dm@gmail.com>
2022-06-02 17:32:12 +0000

    demo

--- Jeux/jeux.js
+++ Jeux/jeux.js
@@ -1,3 +1,4 @@
+ /* 
 let ques = document.querySelector("#quest1")
 let bloc1 = document.querySelector(".block1")
 let count = document.querySelector('#countdouwn')
@@ -8,61 +9,99 @@ let score = document.querySelector('#score')
 let time = 60;
 
 
-
-function temps()
-    {
-        time -= 1;
-        count.innerHTML = time;
-
-    if(time == 0 ){
-        time = 60
-        nextQuestions()
-    }
-    }
 fetch("./jeux.json")
     .then(response=>{
-       
         return response.json()
     })
     .then(responses=>{
         stocke = responses.questions;
+       
         gestdequetion()
     })
 
+    // Generateur de question
+    function gestdequetion()
+    {
+        //if(indexdepart <= stocke.length){
+            for(let i = 0; i < stocke.length; i++ )
+            {
 
+                bloc1.innerHTML +=` <div id="quest1" class='question'>
+                <h2>${stocke[i].text}</h2><br>
+                <input type="checkbox" value="${stocke[i].options[0]}" name="question1" type="${stocke[i].type}" id="propos1">
+                <label for="propos1">${stocke[i].options[0]} </label><br>
+                <input type="checkbox" value="${stocke[i].options[1]}" name="question2" type="${stocke[i].type}" id="propos2">
+                <label for="propos2"> ${stocke[i].options[1]} </label><br>
+                <input type="checkbox" value=${stocke[i].options[2]}" valid="valid" name="question3" type="${stocke[i].type}" id="propos3">
+                <label for="propos3">${stocke[i].options[2]} </label><br>`
+                
+            }
 
+            //indexdepart +=3
 
-setInterval(temps,1000)
-
-
-
-function nextQuestions()
-{
-    bloc1.innerHTML = ""
-    gestdequetion()
-    clearInterval(setInterval(temps,1000))
-}
-function gestdequetion()
-{
-    for(let i = indexdepart; i<= indexdepart+2; i++ )
+        /* } else
         {
-            bloc1.innerHTML +=` <div id="quest1" class='question'>
-            <h2>${stocke[i].text}</h2><br>
-              <input type="checkbox" value="${stocke[i].options[0]}" name="question1" type="${stocke[i].type}" id="propos1">
-              <label for="propos1">${stocke[i].options[0]} </label><br>
-              <input type="checkbox" value="${stocke[i].options[1]}" name="question2" type="${stocke[i].type}" id="propos2">
-              <label for="propos2"> ${stocke[i].options[1]} </label><br>
-              <input type="checkbox" value=${stocke[i].options[2]}" valid="valid" name="question3" type="${stocke[i].type}" id="propos3">
-              <label for="propos1">${stocke[i].options[2]} </label><br>`
-        }
-        indexdepart += 3
+            bloc1.innerHTML = "Le resultats est"
 
-}
+            
+        }*/
 
-submit.addEventListener('click' , nextQuestions
+    
+ /* 
+// La Fonction decompte
+                function temps()
+                            {
+                                if (time == 0) {
+                                   // alert('stop')
 
-)
-submit.addEventListener('click' , () =>
-{
-    time =60
-})
+                                } else {
+                                    time -= 1;
+                                }
+                                
+                                count.innerHTML = time;
+                            }
+
+
+
+        setInterval(temps,1000)
+
+        // Fonction permettant d'afficher les questions
+ /* 
+            function nextQuestions()
+            {
+                bloc1.innerHTML = ""
+                suite()
+            }
+/////////// La condition pour afficher et pour bloquer le temps
+ /* 
+            function suite()
+            {
+                if(indexdepart<=9)
+                {
+                    gestdequetion()
+                    clearInterval(setInterval(temps,1000))
+                }else
+                {
+                    bloc1.innerHTML ="<h2>LE Resultat est : </h2><br>"
+
+                    
+                }
+
+            }
+
+        submit.addEventListener('click' , nextQuestions)
+
+       submit.addEventListener('click' , () =>
+        {
+            time = 60
+        }) */
+
+
+         let form = document.querySelector('#Forme')
+         let TAbleauresults = [];
+
+         form.addEventListener('submit', (e) =>
+         {
+             e.defaultPrevented()
+             console.log(document.querySelector('input[name="q1":checked').value)
+        }) 
