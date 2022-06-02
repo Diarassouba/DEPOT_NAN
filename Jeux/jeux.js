12fc9aaa644891ef905d55ac973bf7241e654a1b
Diarassouba mamadou <couda.dm@gmail.com>
2022-06-02 08:23:14 +0000

    le code de ouf

---
+++ Jeux/jeux.js
@@ -0,0 +1,85 @@
+let ques = document.querySelector("#quest1")
+let bloc1 = document.querySelector(".block1")
+let count = document.querySelector('#countdouwn')
+let stocke = [];
+let indexdepart = 0
+let submit = document.querySelector('#submit');
+let score = document.querySelector('#score')
+let time = 10;
+
+
+fetch("./jeux.json")
+    .then(response=>{
+       
+        return response.json()
+    })
+    .then(responses=>{
+        stocke = responses.questions;
+        gestdequetion()
+    })
+        // Fonction Temps ecouler  
+        function temps()
+            {
+
+                if (indexdepart<9) {
+                    time -= 1;
+                    count.innerHTML = time;
+        
+                    if(time == 0 ){
+                        time = 10
+                        nextQuestions()
+                    }
+
+                }
+                else{
+
+                   
+                
+                } 
+            }
+
+
+        setInterval(temps,1000)
+
+        function nextQuestions()
+        {
+            bloc1.innerHTML = ""
+            gestdequetion()
+            if (indexdepart<10) {
+                clearInterval(setInterval(temps,1000))
+            }
+            
+        }
+        function gestdequetion()
+        {
+                if (indexdepart<9) {
+                    for(let i = indexdepart; i<= indexdepart+2; i++ )
+                    {
+                        bloc1.innerHTML +=` <div id="quest1" class='question'>
+                        <h2>${stocke[i].text}</h2><br>
+                        <input type="checkbox" value="${stocke[i].options[0]}" name="question1" type="${stocke[i].type}" id="propos1">
+                        <label for="propos1">${stocke[i].options[0]} </label><br>
+                        <input type="checkbox" value="${stocke[i].options[1]}" name="question2" type="${stocke[i].type}" id="propos2">
+                        <label for="propos2"> ${stocke[i].options[1]} </label><br>
+                        <input type="checkbox" value=${stocke[i].options[2]}" valid="valid" name="question3" type="${stocke[i].type}" id="propos3">
+                        <label for="propos1">${stocke[i].options[2]} </label><br>`
+                    
+                    }
+                    indexdepart +=3
+                }else{
+                    bloc1.innerHTML +=` <h2>Resultat</h2>`
+                    count.innerHTML = 0;
+                    console.log("fin")
+                }
+                
+        
+
+        }
+
+        submit.addEventListener('click' , nextQuestions
+
+        )
+        submit.addEventListener('click' , () =>
+        {
+            time =60
+        })
